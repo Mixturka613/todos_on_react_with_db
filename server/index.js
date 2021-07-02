23778d67db64eca09c2a9d18caf33d1ed4dbe7d8
Kori <vladislav.kori@yandex.ru>
2021-07-02 16:23:58 +0300

    starting create server

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const router = require('./router')
+
+const PORT = process.env.PORT || 4000
+const app = express()
+
+app.use(express.json())
+
+app.use('/api/', router)
+
+const startApp = async () => {
+    try {
+        // await mongoose.connect(process.env.URL_DB, { useUnifiedTopology: true, useNewUrlParser: true })
+        app.listen(PORT, () => {console.log(`Server started: http://localhost:${PORT}/`)})
+    } catch(e) {
+        console.log(e)
+    }
+}
+
+startApp()
